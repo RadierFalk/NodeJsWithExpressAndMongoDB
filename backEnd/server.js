5642b36cf5981ba9b1a2c5eec2e2bf806f9d6574
RadierFalk <radierfalcao4@gmail.com>
2022-12-10 11:17:42 -0400

    importando app no server

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,21 +1,7 @@
-// importando o modulo nativo HTTP
-const http = require("http");
+import app from "./src/app";
+
 //definindo porta do computador para hospedar o servidor
 const port = 3000;
-
-const rotas = {
-  "/": "Curso de Node",
-  "/livros": "Entrei na pg de Livros",
-  "/autores": "Listagem de Autores",
-  "/editora": "editoras",
-  "/sobre": "Inf. sobre projeto ",
-};
-
-// constante que cria o servidor e passo os parametros que ele irá responder
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/plain" });
-  res.end(rotas[req.url]);
-});
 
 server.listen(port, () => {
   console.log(`Servidor escutando em http://localhost:${port}`);
