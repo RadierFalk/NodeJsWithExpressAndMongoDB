b3414b2c4725ea24678edcabb7163c2f13fa87b3
RadierFalk <radierfalcao4@gmail.com>
2022-12-10 11:24:24 -0400

    Correções na sintaxe

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,8 +1,8 @@
-import app from "./src/app";
+import app from "./src/app.js";
 
 //definindo porta do computador para hospedar o servidor
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Servidor escutando em http://localhost:${port}`);
 });
