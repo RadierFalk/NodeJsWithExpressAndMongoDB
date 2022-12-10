7e2c37187b6fa25fbae68abe67301c51219c65f0
RadierFalk <radierfalcao4@gmail.com>
2022-12-10 11:48:07 -0400

    Add express.json

--- backEnd/src/app.js
+++ backEnd/src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 const livros = [
   { id: 1, Titulo: "Senhor dos Aneis" },
   { id: 2, Titulo: "Harry Potter e a Pedra Filosofal" },
@@ -12,6 +14,11 @@ app.get("/", (req, res) => {
 });
 app.get("/livros", (req, res) => {
   res.status(200).json(livros);
+});
+
+app.post("/livros", (req, res) => {
+  livros.push(req.body);
+  res.status(201).send("O livro foi cadastrado com sucesso");
 });
 
 export default app;
